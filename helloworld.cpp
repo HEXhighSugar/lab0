d292bb9fe7738011fafca2a31d3d23af21060df9
HEXhighSugar <74313819+HEXhighSugar@users.noreply.github.com>
2024-09-10 13:17:11 +0300

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,22 @@
-#include iostream
+#include <iostream>
+
+using namespace std;
 
 int main() {
-    cout<<"hello world";
+    int Hello_World;
+    int Input_Symbol;
+    return 0;
+}
+
+int Hello_World() {
+    cout << "Hello World!"; 
+    return 0;
+}
+
+int Input_Symbol() {
+    char symbol;
+    cout << "Введите любой символ: ";
+    cin >> symbol;
+    cout << "Заберите ваш символ обратно: " << symbol;
     return 0;
 }
